60cdcba221e88d0d6bcb7882dd31682a38ed8662
Roman Marasanov <Roman_Marasanov@epam.com>
2022-10-19 02:29:35 +0300

    add Application.kt as entry point

---
+++ src/main/kotlin/com/justai/jaicf/spring/Application.kt
@@ -0,0 +1,15 @@
+package com.justai.jaicf.spring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+@EnableConfigurationProperties
+@ConfigurationPropertiesScan("com.justai.jaicf.spring.config")
+class Application
+
+fun main(args: Array<String>) {
+    runApplication<Application>(*args)
+}
