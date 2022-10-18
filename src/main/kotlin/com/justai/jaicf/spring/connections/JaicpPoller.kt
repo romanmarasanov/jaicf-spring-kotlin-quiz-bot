f3b7729f30829b369aa60fde2a9e9c657166c55b
Roman Marasanov <Roman_Marasanov@epam.com>
2022-10-19 02:29:06 +0300

    change JaicpPoller from template: make it spring component

--- src/main/kotlin/com/justai/jaicf/spring/connections/JaicpPoller.kt
+++ src/main/kotlin/com/justai/jaicf/spring/connections/JaicpPoller.kt
@@ -1,20 +1,27 @@
 package com.justai.jaicf.spring.connections
 
+import com.justai.jaicf.api.BotApi
 import com.justai.jaicf.channel.jaicp.JaicpPollingConnector
-import com.justai.jaicf.channel.jaicp.channels.ChatApiChannel
-import com.justai.jaicf.channel.jaicp.channels.ChatWidgetChannel
-import com.justai.jaicf.channel.jaicp.channels.TelephonyChannel
-import com.justai.jaicf.spring.accessToken
-import com.justai.jaicf.spring.templateBot
+import com.justai.jaicf.channel.telegram.TelegramChannel
+import com.justai.jaicf.spring.config.BotConfig
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
+import org.springframework.stereotype.Component
 
-fun main() {
-    JaicpPollingConnector(
-        templateBot,
-        accessToken,
-        channels = listOf(
-            ChatApiChannel,
-            ChatWidgetChannel,
-            TelephonyChannel
-        )
-    ).runBlocking()
+@Component
+class JaicpPoller(
+    private val botApi: BotApi,
+    private val botConfiguration: BotConfig
+): ApplicationRunner, CoroutineScope {
+    override val coroutineContext = Dispatchers.Default
+
+    override fun run(args: ApplicationArguments?) {
+            JaicpPollingConnector(
+                botApi = botApi,
+                accessToken = botConfiguration.accessToken,
+                channels = listOf(TelegramChannel)
+            ).runBlocking()
+    }
 }
